ff19473e5616d7199dc30c21ef5111e865af45d0
Chih-Hsuan Yen <yan12125@gmail.com>
2019-03-14 16:53:38 +0800

    Replace Portfiles inplace and accept filenames as arguments

--- revbump-ports.sh
+++ revbump-ports.sh
@@ -11,28 +11,21 @@ if ! which gsed >/dev/null; then
 	echo >&2 "ERROR: This script needs gsed, please install the gsed port"
 	exit 2
 fi
-if ! which colordiff >/dev/null; then
-	echo >&2 "ERROR: This script needs colordiff, please install the colordiff port"
-	exit 2
-fi
-
-for portfile in $(port file "$@" | sort -u); do
+for portfile in "$@"; do
 	if grep -Eq $'^[ \t]*revision[ \t]+[0-9]+' "$portfile"; then
-		contents=$(gsed -rf increase-revision.sed "$portfile")
+		contents=$(gsed -i -rf increase-revision.sed "$portfile")
 	else
 		if grep -Eq $'^[ \t]*version[ \t]+.*$' "$portfile"; then
-			contents=$(gsed -rf add-revision.sed "$portfile")
+			contents=$(gsed -i -rf add-revision.sed "$portfile")
 		elif grep -Eq $'^[ \t]*github.setup[ \t]+.*$' "$portfile"; then
-			contents=$(gsed -rf add-revision-github.sed "$portfile")
+			contents=$(gsed -i -rf add-revision-github.sed "$portfile")
 		elif grep -Eq $'^[ \t]*perl5.setup[ \t]+.*$' "$portfile"; then
-			contents=$(gsed -rf add-revision-perl5.sed "$portfile")
+			contents=$(gsed -i -rf add-revision-perl5.sed "$portfile")
 		else
 			noversion+=("$portfile")
 			continue
 		fi
 	fi
-
-	colordiff -u "$portfile" <(cat <<<"${contents}")
 done
 
 if [ ${#noversion[@]} -gt 0 ]; then
